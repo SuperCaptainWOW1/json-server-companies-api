d750bfdf9e015fe83501af9b1bf23dd3cc4e2fb5
Vladimir Makhortov <makhortov.dev@gmail.com>
2023-05-14 21:09:56 +0300

    fix

--- server.js
+++ server.js
@@ -10,4 +10,10 @@ const port = process.env.PORT || 3500;
 server.use(middlewares);
 server.use(router);
 
+server.all("/", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 server.listen(port);
