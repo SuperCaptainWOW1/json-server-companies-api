dd574a38f0158f315c663dae308de4c663fc3516
Vladimir Makhortov <makhortov.dev@gmail.com>
2023-05-14 19:54:56 +0300

    Fix db is read-only

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+// const router = jsonServer.router("db.json");
+const fs = require("fs");
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
 
