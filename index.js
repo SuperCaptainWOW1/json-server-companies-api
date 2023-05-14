ac6d654556602346dadde3c2f05d760842061026
Vladimir Makhortov <makhortov.dev@gmail.com>
2023-05-14 20:42:56 +0300

    fix

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-const fs = require("fs");
-const db = JSON.parse(fs.readFileSync("db.json"));
-const router = jsonServer.router(db);
+const router = jsonServer.router("db.json");
+// const fs = require("fs");
+// const db = JSON.parse(fs.readFileSync("db.json"));
+// const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3500;
 
